237a73fb3fbb056ebb9e0bb67291069f77627304
Ninuq <helena.rosenzw@gmail.com>
2017-05-16 19:53:04 +0200

    functioning html iterator

--- lab6/WebLinks.java
+++ lab6/WebLinks.java
@@ -27,13 +27,20 @@ public void loadWebPage() throws IOException{
        htmlDoc.putProperty("IgnoreCharsetDirective", Boolean.TRUE);
 //       htmlDoc = (HTMLDocument) htmlKit.createDefaultDocument();
 
-      // while(reader.ready()){
+      // while(reader.ready())
     //   System.out.println("Kom in i while loopen");
        System.out.println((char)reader.read());
+       // Lägger in inputStream i ett html dokument
        htmlKit.read(reader, htmlDoc, 0); //BadLocationException kopplat till read
-       System.out.println(htmlDoc);
-    //   }
-   }
+  //     System.out.println(htmlDoc);
+
+
+       for(HTMLDocument.Iterator htmlIterator = htmlDoc.getIterator(HTML.Tag.A); htmlIterator.isValid(); htmlIterator.next()){
+              AttributeSet attributes = htmlIterator.getAttributes();
+              String attributesHREF = (String) attributes.getAttribute(HTML.Attribute.HREF);
+  			      System.out.println(attributesHREF);
+       }
+    }
    catch(BadLocationException | IOException e){
        System.out.println("Failed to load web page.");
    }
@@ -46,7 +53,6 @@ public static void main(String[] args){
     }
     catch(IOException e){
         System.out.println("Failed to load web page.");
-    }
-
   }
+}
 }
